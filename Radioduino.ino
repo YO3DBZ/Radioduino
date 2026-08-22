8d8962f97024d18aae111a4038d1da4df5f4b50e
alteris <cristea.matei@qbic.ro>
2026-08-22 22:41:33 +0300

    added a swich() instead of a bunch of ifs

--- Radioduino.ino
+++ Radioduino.ino
@@ -281,14 +281,14 @@ void setup() {
   Serial.begin (9600);
   currentTime = millis();
   loopTime = currentTime;
-  //Serial.print("Let's get started - Rotary Encoder is at position ");
-  //Serial.print(counter);
   Serial.println();
   bool i2c_found;
   i2c_found = si5351.init(SI5351_CRYSTAL_LOAD_8PF, 0, 0);
   if(!i2c_found)
   {
-    Serial.println("Device not found on I2C bus!");
+    while(true){
+      Serial.println("Device not found on I2C bus!");
+    }
   }
   si5351.set_freq(freq, SI5351_CLK0);
   // initialize the lcd
@@ -299,7 +299,6 @@ void setup() {
   lcd.print("test");
   //ch_freq();
   vfo_update();
-  
 }
 
 // 0 for no 1 for clockwise 2 for counterclockwise
@@ -379,310 +378,307 @@ void loop() {
   }
   //meniu logic
   if(vfo == false){
-    // exit meniu logic 
-    if(meniu == 0){
-      if (digitalRead(8)== LOW){
-        while(digitalRead(8)== LOW){
-        }
-      vfo = true;
-      vfo_update();
-      }
-    }
-    // step select
-    if (meniu == 1){
-      if (digitalRead(8)== LOW){
-        while(digitalRead(8)== LOW){
-        }
-        delay(50);
-
-        if(modifying == false){
-          modifying = true;
-        }
-        else
-        {
-          modifying = false;
-        }
-        meniu_update();
-      }
-      if (r !=0 && modifying == true){
-        if (r==1){
-          step_size = step_size * 10ULL;
-          if (step_size > 10000000ULL) step_size = 100000000ULL; // 1Mhz max
-        }
-        if (r==2){
-          step_size = step_size / 10ULL;
-          if (step_size < 100ULL) step_size = 100ULL;// 1 Hz min
-        }
-        meniu_update();
-      }
-    }
-    // mode select
-    if (meniu == 2){
-      if (digitalRead(8)== LOW){
-        while(digitalRead(8)== LOW){
-        }
-        delay(50);
-
-        if(modifying == false){
-          modifying = true;
-        }
-        else
-        {
-          modifying = false;
-        }
-        meniu_update();
-      }
-      if (r != 0 && modifying == true){
-        if(r == 1){
-          mode++;
-          if (mode >= 4){
-            mode = 1;
+    switch(meniu){
+      // exit meniu logic 
+      case 0:
+        if (digitalRead(8)== LOW){
+          while(digitalRead(8)== LOW){
           }
+        vfo = true;
+        vfo_update();
         }
-        if (r==2){
-          mode--;
-          if(mode <= 0){
-            mode = 3;
+        break;
+      // step select
+      case 1:
+        if (digitalRead(8)== LOW){
+          while(digitalRead(8)== LOW){
           }
-        }
-        meniu_update();
-        if(mode == 1){
-          if_shift = usb_shift;
-        }else if(mode == 2){
-          if_shift = lsb_shift;
-        }else if(mode == 3){
-          if_shift = cw_shift;
-        }
-      }
-    } 
-    //if1_enable on off
-    if (meniu == 3){
-      if (digitalRead(8)== LOW){
-        while(digitalRead(8)== LOW){
-        }
-        delay(50);
+          delay(50);
 
-        if(modifying == false){
-          modifying = true;
-        }
-        else
-        {
-          modifying = false;
-        }
-        meniu_update();
-      }
-      if(modifying== true && r!=0){
-        if(r == 1){
-          if(if1_enable == false){
-            if1_enable= true;
+          if(modifying == false){
+            modifying = true;
           }
-          else
-          {
-            if1_enable = false;
+          else{
+            modifying = false;
           }
-        }else{
-          if(if1_enable == false){
-            if1_enable= true;
-          }
-          else
-          {
-            if1_enable = false;
-          }
+          meniu_update();
         }
-        ch_freq(true);
-        ch_freq(false);
-        meniu_update();
-      }
-    }
-    // if1 freq set
-    if(meniu == 4){
-      if (digitalRead(8)== LOW){
-        while(digitalRead(8)== LOW){
-        }
-        delay(50);
-
-        if(modifying == false){
-          modifying = true;
-        }
-        else
-        {
-          modifying = false;
-        }
-        meniu_update();
-      }
-      if (r !=0 && modifying == true){
-        if (r==1){
-          freq_if1 = freq_if1 + step_size;
-        }
-        if (r==2){
-          freq_if1 = freq_if1 - step_size;
-          if (freq_if1 < 100ULL) freq_if1 = 10000ULL;// 100 Hz min
-        }
-        ch_freq(true);
-        ch_freq(false);
-        meniu_update();
-      }
-    }
-    //usb shift
-    if(meniu == 5){
-      if (digitalRead(8)== LOW){
-        while(digitalRead(8)== LOW){
-        }
-        delay(50);
-
-        if(modifying == false){
-          modifying = true;
-        }
-        else
-        {
-          modifying = false;
-        }
-        meniu_update();
-      }
-      if (r !=0 && modifying == true){
-        if (r==1){
-          usb_shift = usb_shift + step_size;
-        }
-        if (r==2){
-          usb_shift = usb_shift - step_size;
-          //if (freq_if1 < 100ULL) freq_if1 = 10000ULL;// 100 Hz min
-        }
-        ch_freq(true);
-        ch_freq(false);
-        meniu_update();
-      }
-      
-    }
-    //lsb shift
-    if(meniu == 6){
-      if (digitalRead(8)== LOW){
-        while(digitalRead(8)== LOW){
-        }
-        delay(50);
-
-        if(modifying == false){
-          modifying = true;
-        }
-        else
-        {
-          modifying = false;
-        }
-        meniu_update();
-      }
-      if (r !=0 && modifying == true){
-        if (r==1){
-          lsb_shift = lsb_shift + step_size;
-        }
-        if (r==2){
-          lsb_shift = lsb_shift - step_size;
-          //if (freq_if1 < 100ULL) freq_if1 = 10000ULL;// 100 Hz min
-        }
-        ch_freq(true);
-        ch_freq(false);
-        meniu_update();
-      }
-      
-    }
-    //cw shift
-    if(meniu == 7){
-      if (digitalRead(8)== LOW){
-        while(digitalRead(8)== LOW){
-        }
-        delay(50);
-
-        if(modifying == false){
-          modifying = true;
-        }
-        else
-        {
-          modifying = false;
-        }
-        meniu_update();
-      }
-      if (r !=0 && modifying == true){
-        if (r==1){
-          cw_shift = cw_shift + step_size;
-        }
-        if (r==2){
-          cw_shift = cw_shift - step_size;
-          //if (freq_if1 < 100ULL) freq_if1 = 10000ULL;// 100 Hz min
-        }
-        ch_freq(true);
-        ch_freq(false);
-        meniu_update();
-      }
-    }
-    //bfo on off
-    if(meniu == 8){
-      if (digitalRead(8)== LOW){
-        while(digitalRead(8)== LOW){
-        }
-        delay(50);
-
-        if(modifying == false){
-          modifying = true;
-        }
-        else
-        {
-          modifying = false;
-        }
-        meniu_update();
-      }
-      if (r != 0 && modifying == true){
-        if(r == 1){
-          BFO_state++;
-          if (BFO_state >= 3){
-            BFO_state = 0;
-          }
-        }
-        if (r==2){
-          BFO_state--;
-          if(BFO_state <= -1){
-            BFO_state = 2;
-          }
-        }
-        if(BFO_state >= 1){
-          si5351.output_enable(SI5351_CLK2, 1);
-        }else{
-          si5351.output_enable(SI5351_CLK2, 0);
-        }
-        if(BFO_state == 2){
-          BFO_freq = freq_if1;
-        }
-        bfo_update();
-        meniu_update();
-      }
-    }
-    // bfo freq set
-    if(meniu == 9){
-      if (digitalRead(8)== LOW){
-        while(digitalRead(8)== LOW){
-        }
-        delay(50);
-
-        if(modifying == false){
-          modifying = true;
-        }
-        else
-        {
-          modifying = false;
-        }
-        meniu_update();
-      }
-      if(BFO_state !=2 ){
         if (r !=0 && modifying == true){
           if (r==1){
-            BFO_freq= BFO_freq + step_size;
-            if (BFO_freq > 10000000000ULL) BFO_freq = 10000000000ULL; // 100Mhz max
+            step_size = step_size * 10ULL;
+            if (step_size > 10000000ULL) step_size = 100000000ULL; // 1Mhz max
           }
           if (r==2){
-            BFO_freq = BFO_freq - step_size;
-            if (BFO_freq < 5000000ULL) BFO_freq = 5000000ULL; // 50Khz max
+            step_size = step_size / 10ULL;
+            if (step_size < 100ULL) step_size = 100ULL;// 1 Hz min
+          }
+          meniu_update();
+        }
+        break;
+      // mode select
+      case 2:
+        if (digitalRead(8)== LOW){
+          while(digitalRead(8)== LOW){
+          }
+          delay(50);
+
+          if(modifying == false){
+            modifying = true;
+          }
+          else
+          {
+            modifying = false;
+          }
+          meniu_update();
+        }
+        if (r != 0 && modifying == true){
+          if(r == 1){
+            mode++;
+            if (mode >= 4){
+              mode = 1;
+            }
+          }
+          if (r==2){
+            mode--;
+            if(mode <= 0){
+              mode = 3;
+            }
+          }
+          meniu_update();
+          if(mode == 1){
+            if_shift = usb_shift;
+          }else if(mode == 2){
+            if_shift = lsb_shift;
+          }else if(mode == 3){
+            if_shift = cw_shift;
+          }
+        }
+        break; 
+      //if1_enable on off
+      case 3:
+        if (digitalRead(8)== LOW){
+          while(digitalRead(8)== LOW){
+          }
+          delay(50);
+
+          if(modifying == false){
+            modifying = true;
+          }
+          else
+          {
+            modifying = false;
+          }
+          meniu_update();
+        }
+        if(modifying== true && r!=0){
+          if(r == 1){
+            if(if1_enable == false){
+              if1_enable= true;
+            }
+            else
+            {
+              if1_enable = false;
+            }
+          }else{
+            if(if1_enable == false){
+              if1_enable= true;
+            }
+            else
+            {
+              if1_enable = false;
+            }
+          }
+          ch_freq(true);
+          ch_freq(false);
+          meniu_update();
+        }
+        break;
+      // if1 freq set
+      case 4:
+        if (digitalRead(8)== LOW){
+          while(digitalRead(8)== LOW){
+          }
+          delay(50);
+
+          if(modifying == false){
+            modifying = true;
+          }
+          else
+          {
+            modifying = false;
+          }
+          meniu_update();
+        }
+        if (r !=0 && modifying == true){
+          if (r==1){
+            freq_if1 = freq_if1 + step_size;
+          }
+          if (r==2){
+            freq_if1 = freq_if1 - step_size;
+            if (freq_if1 < 100ULL) freq_if1 = 10000ULL;// 100 Hz min
+          }
+          ch_freq(true);
+          ch_freq(false);
+          meniu_update();
+        }
+        break;
+      //usb shift
+      case 5:
+        if (digitalRead(8)== LOW){
+          while(digitalRead(8)== LOW){
+          }
+          delay(50);
+
+          if(modifying == false){
+            modifying = true;
+          }
+          else
+          {
+            modifying = false;
+          }
+          meniu_update();
+        }
+        if (r !=0 && modifying == true){
+          if (r==1){
+            usb_shift = usb_shift + step_size;
+          }
+          if (r==2){
+            usb_shift = usb_shift - step_size;
+            //if (freq_if1 < 100ULL) freq_if1 = 10000ULL;// 100 Hz min
+          }
+          ch_freq(true);
+          ch_freq(false);
+          meniu_update();
+        }
+        break;
+      //lsb shift
+      case 6:
+        if (digitalRead(8)== LOW){
+          while(digitalRead(8)== LOW){
+          }
+          delay(50);
+
+          if(modifying == false){
+            modifying = true;
+          }
+          else
+          {
+            modifying = false;
+          }
+          meniu_update();
+        }
+        if (r !=0 && modifying == true){
+          if (r==1){
+            lsb_shift = lsb_shift + step_size;
+          }
+          if (r==2){
+            lsb_shift = lsb_shift - step_size;
+            //if (freq_if1 < 100ULL) freq_if1 = 10000ULL;// 100 Hz min
+          }
+          ch_freq(true);
+          ch_freq(false);
+          meniu_update();
+        }
+        break;
+      //cw shift
+      case 7:
+        if (digitalRead(8)== LOW){
+          while(digitalRead(8)== LOW){
+          }
+          delay(50);
+          if(modifying == false){
+            modifying = true;
+          }
+          else
+          {
+            modifying = false;
+          }
+          meniu_update();
+        }
+        if (r !=0 && modifying == true){
+          if (r==1){
+            cw_shift = cw_shift + step_size;
+          }
+          if (r==2){
+            cw_shift = cw_shift - step_size;
+            //if (freq_if1 < 100ULL) freq_if1 = 10000ULL;// 100 Hz min
+          }
+          ch_freq(true);
+          ch_freq(false);
+          meniu_update();
+        }
+        break;
+      //bfo on off
+      case 8:
+        if (digitalRead(8)== LOW){
+          while(digitalRead(8)== LOW){
+          }
+          delay(50);
+          if(modifying == false){
+            modifying = true;
+          }
+          else
+          {
+            modifying = false;
+          }
+          meniu_update();
+        }
+        if (r != 0 && modifying == true){
+          if(r == 1){
+            BFO_state++;
+            if (BFO_state >= 3){
+              BFO_state = 0;
+            }
+          }
+          if (r==2){
+            BFO_state--;
+            if(BFO_state <= -1){
+              BFO_state = 2;
+            }
+          }
+          if(BFO_state >= 1){
+            si5351.output_enable(SI5351_CLK2, 1);
+          }else{
+            si5351.output_enable(SI5351_CLK2, 0);
+          }
+          if(BFO_state == 2){
+            BFO_freq = freq_if1;
           }
           bfo_update();
           meniu_update();
         }
-      }
+        break;
+      // bfo freq set
+      case 9:
+        if (digitalRead(8)== LOW){
+          while(digitalRead(8)== LOW){
+          }
+          delay(50);
+
+          if(modifying == false){
+            modifying = true;
+          }
+          else
+          {
+            modifying = false;
+          }
+          meniu_update();
+        }
+        if(BFO_state !=2 ){
+          if (r !=0 && modifying == true){
+            if (r==1){
+              BFO_freq= BFO_freq + step_size;
+              if (BFO_freq > 10000000000ULL) BFO_freq = 10000000000ULL; // 100Mhz max
+            }
+            if (r==2){
+              BFO_freq = BFO_freq - step_size;
+              if (BFO_freq < 5000000ULL) BFO_freq = 5000000ULL; // 50Khz max
+            }
+            bfo_update();
+            meniu_update();
+          }
+        }
+        break;
     }
   }
 }
